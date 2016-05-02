b83a5e01cf2cde4787cfbd99923708689b665edf
Nicholas Roberts <niccolo.roberts@gmail.com>
2016-05-02 14:00:13 -0700

    Ansible from ppa and python-crypto and requirements.yml tidy

--- tests/setup.sh
+++ tests/setup.sh
@@ -7,7 +7,12 @@
 #sudo rm -rf /var/lib/mysql
 #sudo rm -rf /root/.my.cnf
 
-curl https://raw.githubusercontent.com/ErgonLogicEnterprises/ansible-bootstrap/master/install-ansible.sh | /bin/sh
+sudo apt-get install git python-setuptools python-dev python-pip python-crypto -yq
+sudo apt-get install software-properties-common -yq
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get install ansible -y
+#curl https://raw.githubusercontent.com/ErgonLogicEnterprises/ansible-bootstrap/master/install-ansible.sh | /bin/sh
 sudo ansible-galaxy install -r /vagrant/tests/requirements.yml
 target=/etc/ansible/roles/ansible-role-aegir3drulenium
 if [ ! -e $target ]; then
